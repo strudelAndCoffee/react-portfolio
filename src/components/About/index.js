fedc5da4a19f294b9b2bd2218bf29a6fd926e0da
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-16 04:16:46 -0500

    finished resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,35 @@
 // ABOUT
 
-import React from 'react';
+import React from "react";
 
 function About() {
-    return(
-        <section className="About">
-            <form>
-                <fieldset>
-                    <legend>About Stevie</legend>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad laborum saepe tempore cumque laboriosam, repudiandae voluptas ducimus totam nisi, possimus rerum magnam, perspiciatis voluptate distinctio accusamus! Minus, illo a.</p>
-                </fieldset>
-            </form>
-        </section>
-    );
-};
+  return (
+    <section className="About">
+      <form>
+        <fieldset>
+          <legend>About Stevie</legend>
+          <div className="bio-text">
+            I am currently studying to become a full stack web developer at The University of Texas in Austin.
+            <br />
+            <br />
+            Austin has been my home since birth, and it is my dream to work, play, and create here while living
+            a full and rewarding life in the city I love.
+            <br />
+            <br />
+            I have been a musician and audio recording/mixing engineer for over a decade, and will never retire
+            from pursuing those passions, but my work ethic and ambition reaches far beyond the scope working
+            professionally in the music business.
+            <br />
+            <br />
+            This is why I am devoting my time and intellect to grow and excel in the diverse field of web
+            development. I have learned that designing and building websites is as engaging and creatively
+            stimulating an experience as performing music!
+            <br />
+          </div>
+        </fieldset>
+      </form>
+    </section>
+  );
+}
 
 export default About;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,8 +7,24 @@ function Resume() {
         <section className="Resume">
             <form>
                 <fieldset>
-                    <legend>Resume</legend>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad laborum saepe tempore cumque laboriosam, repudiandae voluptas ducimus totam nisi, possimus rerum magnam, perspiciatis voluptate distinctio accusamus! Minus, illo a.</p>
+                    <legend>Full Stack Web Development</legend>
+                    <div className="bio-text">
+                        <h3>Languages / Technologies</h3>
+                        <ul>
+                            <li>JavaScript (ES6) / HTML / CSS</li>
+                            <li>3rd Party APIs / Frameworks (Bootstrap, Bulma, jQuery, etc.)</li>
+                            <li className="italic">Comprehending Documentation</li>
+                            <li>Node.JS / Express.JS / AJAX</li>
+                            <li>SQL / MongoDB</li>
+                            <li>React</li>
+                        </ul>
+                        <h3>Skills</h3>
+                        <ul>
+                            <li>AGILE / OOP / TDD</li>
+                            <li>Model View Controller / Progressive Web Application</li>
+                            <li>MERN Stack</li>
+                        </ul>
+                    </div>
                 </fieldset>
             </form>
         </section>
