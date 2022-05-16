0e0f7c6e71d5733743e9fb16388b7dd5160a78ae
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-16 00:50:55 -0500

    set up contact section, sized form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,21 @@ import React from 'react';
 function Contact() {
     return(
         <section className="Contact">
-            Contact Stevie
+            <form id="contact-form">
+                <fieldset className="input-container">
+                    <legend>Contact Stevie</legend>
+                    <label for="name">Name </label>
+                    <input type="text" id="name" className="form-input" />
+                    <br />
+                    <label for="email">Email </label>
+                    <input type="email" id="email" className="form-input" />
+                    <br />
+                    <label for="message">Message</label>
+                    <textarea id="message" className="form-text"></textarea>
+                    <br />
+                    <button type="submit" id="form-submit" className="form-btn">Submit</button>
+                </fieldset>
+            </form>
         </section>
     );
 };
