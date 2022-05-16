9b399e76e7b1966c63172e682829593aa1309868
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-16 15:41:36 -0500

    updated About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,18 +12,18 @@ function About() {
             I am currently studying to become a full stack web developer at The University of Texas in Austin.
             <br />
             <br />
-            Austin has been my home since birth, and it is my dream to work, play, and create here while living
-            a full and rewarding life in the city I love.
+            Austin has been my home since birth, and it is my dream to continue to work, play, and create here 
+            in the city that I love.
             <br />
             <br />
             I have been a musician and recording/mixing engineer for over a decade, and will never retire
             from pursuing those passions, but my work ethic and ambition pushes beyond the scope that working
-            professionally in the music business offers.
+            in the music business offers.
             <br />
             <br />
             This is why I am devoting my time and intellect to grow and excel in the diverse field of web
-            development! I have learned that designing and building websites is as an engaging and creatively
-            stimulating experience as performing music.
+            development. I have learned that designing and building websites is as an engaging and creatively
+            stimulating experience as performing music!
             <br />
           </div>
         </fieldset>
