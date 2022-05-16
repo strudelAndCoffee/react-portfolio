1690e5a7ba6d89bf72f8175d530203d7ba4f5c4f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-16 06:25:54 -0500

    moved app photos to src

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,42 +7,42 @@ function Portfolio() {
     // app data array
     const [apps] = useState([
         {
-            imgSrc: '/assets/images/travel-diary.png',
+            imgSrc: 'travel-diary.png',
             imgAlt: 'homepage for travel diary.',
             appTitle: 'Travel.Diary',
             appUrl: 'https://afternoon-harbor-62644.herokuapp.com/',
             githubUrl: 'https://github.com/strudelAndCoffee/travel-log-dashboard'
         },
         {
-            imgSrc: '/assets/images/dev-dash.png',
+            imgSrc: 'dev-dash.png',
             imgAlt: 'homepage for dev dash.',
             appTitle: 'Dev Dash',
             appUrl: 'https://cryptic-peak-17894.herokuapp.com/',
             githubUrl: 'https://github.com/strudelAndCoffee/dev-dash'
         },
         {
-            imgSrc: '/assets/images/git-it-done.png',
+            imgSrc: 'git-it-done.png',
             imgAlt: 'homepage for git it done.',
             appTitle: 'Git It Done!',
             appUrl: 'https://strudelandcoffee.github.io/git-it-done/',
             githubUrl: 'https://github.com/strudelAndCoffee/git-it-done'
         },
         {
-            imgSrc: '/assets/images/spacex.png',
+            imgSrc: 'spacex.png',
             imgAlt: 'homepage for SpaceX launch info dashboard.',
             appTitle: 'SpaceX Launch Info',
             appUrl: 'https://strudelandcoffee.github.io/group-project/',
             githubUrl: 'https://github.com/strudelAndCoffee/group-project'
         },
         {
-            imgSrc: '/assets/images/note-taker.png',
+            imgSrc: 'note-taker.png',
             imgAlt: 'homepage for note taker app.',
             appTitle: 'Note Taker App',
             appUrl: 'https://do-ly-noted.herokuapp.com/',
             githubUrl: 'https://github.com/strudelAndCoffee/do-ly-noted'
         },
         {
-            imgSrc: '/assets/images/code-quiz.png',
+            imgSrc: 'code-quiz.png',
             imgAlt: 'homepage for do you even code quiz app',
             appTitle: 'Do You Even Code? Quiz',
             appUrl: 'https://strudelandcoffee.github.io/do-you-even-code-quiz/',
