da8a37c06479cbc452060589c0b57d912e66068a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-10 13:38:02 -0500

    npm init on root folder

--- server/contact-route.js
+++ server/contact-route.js
@@ -1,15 +1,6 @@
 const router = require('express').Router();
 const { Contact, ReadMore } = require('./models');
 
-router.get('/contact', (req, res) => {
-    Contact.findAll()
-    .then(data => res.json(data))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
-
 router.post('/contact', (req, res) => {
     Contact.create({
         name: req.body.name,
@@ -23,56 +14,9 @@ router.post('/contact', (req, res) => {
     });
 });
 
-router.delete('/contact/:id', (req, res) => {
-    Contact.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(data => {
-        if (!data) {
-            res.status(404).json({ message: "No contact found with that ID." });
-            return;
-        }
-        res.json(data)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.get('/readmore', (req, res) => {
-    ReadMore.findAll()
-    .then(data => res.json(data))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
-
 router.post('/readmore', (req, res) => {
     ReadMore.create()
     .then(data => res.json(data))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.delete('/readmore/:id', (req, res) => {
-    ReadMore.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(data => {
-        if (!data) {
-            res.status(404).json({ message: "No readmore found with that ID." });
-            return;
-        }
-        res.json(data)
-    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
