fdb577b5ac261466e30def3ec7275e0cff072b99
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-10 15:43:21 -0500

    fixed route paths

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,17 +6,17 @@ import { Link, NavLink } from 'react-router-dom';
 function Header() {
     return(
         <header>
-            <Link to='/react-portfolio/'>
+            <Link to='/'>
                 <h1>
                     Stephen Trudell
                 </h1>
             </Link>
             <nav>
-                <NavLink to='/react-portfolio/portfolio' className='nav-link' activeClassName='nav-link active'>Portfolio</NavLink>
-                <NavLink to='/react-portfolio/about' className='nav-link' activeClassName='nav-link active'>About</NavLink>
-                <NavLink to='/react-portfolio/resume' className='nav-link' activeClassName='nav-link active'>Resume</NavLink>
-                <NavLink to='/react-portfolio/community' className='nav-link' activeClassName='nav-link active'>Community</NavLink>
-                <NavLink to='/react-portfolio/contact' className='nav-link' activeClassName='nav-link active'>Contact</NavLink>
+                <NavLink to='/portfolio' className='nav-link' activeClassName='nav-link active'>Portfolio</NavLink>
+                <NavLink to='/about' className='nav-link' activeClassName='nav-link active'>About</NavLink>
+                <NavLink to='/resume' className='nav-link' activeClassName='nav-link active'>Resume</NavLink>
+                <NavLink to='/community' className='nav-link' activeClassName='nav-link active'>Community</NavLink>
+                <NavLink to='/contact' className='nav-link' activeClassName='nav-link active'>Contact</NavLink>
             </nav>
         </header>
     );
