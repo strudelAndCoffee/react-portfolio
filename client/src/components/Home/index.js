d29e9dc21d26e97cd2aa6d8a1c4b8ca460dce15b
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-09 19:28:08 -0500

    deleted old client files

--- client/src/components/Home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-    <section className="Home">
-        <h2 className="welcome">Welcome!</h2>
-    </section>
-);
-
-export default Home;
--- client/src/components/NoMatch/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NoMatch = () => {
-  return <div className="nomatch">Oops, we couldn't find that page.</div>;
-};
-
-export default NoMatch;
