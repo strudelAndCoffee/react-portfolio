483e60d2ded88477729d7a058dd7a6ec302a57b1
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-26 00:25:18 -0500

    added get routes

--- server/contact-route.js
+++ server/contact-route.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 const { Contact, ReadMore } = require('./models');
 
+router.get('/readmore', (req, res) => {
+    ReadMore.findAll()
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/contact', (req, res) => {
+    Contact.findAll()
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 router.post('/contact', (req, res) => {
     Contact.create({
         name: req.body.name,
