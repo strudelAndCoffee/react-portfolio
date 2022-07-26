83190b604b64a62ab90316410ef20910e9dd9f05
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-26 00:52:09 -0500

    added link click handlers in project

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -13,19 +13,65 @@ function Project(props) {
         githubUrl
     } = props;
 
+    function handleAppLinkClick(e) {
+        e.preventDefault();
+
+        try {
+            fetch('/api/readmore', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: { type: 'project-app-link' }
+            })
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    function handleGithubClick(e) {
+        e.preventDefault();
+
+        try {
+            fetch('/api/readmore', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: { type: 'project-github-link' }
+            })
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return(
         <div className="Project">
-            <a className="img-link" href={appUrl} target="_blank" rel="noreferrer">
+            <a
+                className="img-link"
+                href={appUrl}
+                target="_blank"
+                rel="noreferrer"
+                onClick={handleAppLinkClick}
+            >
                 <img 
                     src={require(`../../assets/images/${imgSrc}`)}
                     alt={imgAlt}
                 />
             </a>
-            <a className="app-title" href={appUrl} target="_blank" rel="noreferrer">
+            <a
+                className="app-title"
+                href={appUrl}
+                target="_blank"
+                rel="noreferrer"
+                onClick={handleAppLinkClick}
+            >
                 {appTitle}
             </a>
             <br />
-            <a className="link" href={githubUrl} target="_blank" rel="noreferrer">
+            <a
+                className="link"
+                href={githubUrl}
+                target="_blank"
+                rel="noreferrer"
+                onClick={handleGithubClick}
+            >
                 <IconContext.Provider value={{ size: '1em' }}>
                     <GoMarkGithub />
                 </IconContext.Provider>
