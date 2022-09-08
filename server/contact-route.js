144fc3293aa837177d6e4b15fc533a5692763cb0
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-09-08 01:58:04 -0500

    installed nodemailer

--- server/contact-route.js
+++ server/contact-route.js
@@ -1,24 +1,6 @@
 const router = require('express').Router();
 const { Contact, ReadMore } = require('./models');
 
-router.get('/readmore', (req, res) => {
-    ReadMore.findAll()
-    .then(data => res.json(data))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.get('/contact', (req, res) => {
-    Contact.findAll()
-    .then(data => res.json(data))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
 router.post('/contact', (req, res) => {
     Contact.create({
         name: req.body.name,
